dccc7b6b37bfe37e901aa58119188187bde7d5bb
AnatolyUss <anatolyuss@gmail.com>
2015-09-14 01:29:32 +0300

    add explicit case for zero in double column to sanitizeValue method

--- migration/FromMySqlToPostgreSql/FromMySqlToPostgreSql.php
+++ migration/FromMySqlToPostgreSql/FromMySqlToPostgreSql.php
@@ -485,6 +485,7 @@ class FromMySqlToPostgreSql
         switch ($strValue) {
             case '0':
                 return '0';
+            
             case '0000-00-00 00:00:00':
             case '0000-00-00':
                 return '-INFINITY';
